db2d02eb8c8cceff753ff40229929fa241e228fc
Shizcow <pohl.devin@gmail.com>
2020-11-19 12:20:47 -0700

    Fixed hello example

--- examples/hello.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    println!("{}", emoji::food_and_drink::food_marine::CRAB.glyph);
-    println!("{}", emoji::food_and_drink::food_marine::CRAB.codepoint);
-    println!("{}", emoji::food_and_drink::food_marine::CRAB.name);
-    println!("{}", emoji::food_and_drink::food_marine::CRAB.status);
-    println!("{}", emoji::food_and_drink::food_marine::CRAB.introduction_version);
-
-    println!("{}", emoji::test::A);
-}
