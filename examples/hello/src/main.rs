c041f436059a6b74ffcd61d8f4aa9d4dd14d3e1e
Shizcow <pohl.devin@gmail.com>
2020-11-19 21:59:23 -0700

    Updated example

--- examples/hello/src/main.rs
+++ examples/hello/src/main.rs
@@ -6,5 +6,15 @@ fn main() {
 	     emoji::food_and_drink::food_marine::CRAB.status,
 	     emoji::food_and_drink::food_marine::CRAB.introduction_version);
     println!("This emoji has the following annotations associated with it: {:?}",
-	     emoji::food_and_drink::food_marine::CRAB.annotations);
+	     emoji::food_and_drink::food_marine::CRAB.annotations.iter()
+	     .find(|a| a.lang == "en").unwrap().keywords);
+    println!("In Spanish, it's called {}",
+	     emoji::food_and_drink::food_marine::CRAB.annotations.iter()
+	     .find(|a| a.lang == "es").unwrap().tts.unwrap());
+    println!("In Finnish, it's called {}",
+	     emoji::food_and_drink::food_marine::CRAB.annotations.iter()
+	     .find(|a| a.lang == "fi").unwrap().tts.unwrap());
+    println!("In Turkic, it's called {}",
+	     emoji::food_and_drink::food_marine::CRAB.annotations.iter()
+	     .find(|a| a.lang == "uz").unwrap().tts.unwrap());
 }
